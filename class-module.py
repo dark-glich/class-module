ef365b8a1f607df807514e56e1e2c40b4ad3a724
dark-glich <mohdbilal9808464636@gmail.com>
2020-10-16 22:04:50 +0530

    file created
    file added

--- class-module.py
+++ class-module.py
@@ -1,16 +1,34 @@
 
 class employe:
+
+    number_of_employees = -1
+
     def __init__(self, name, age, salary, working_days):
         self.name = name
         self.age = age
         self.salary = salary
         self.working_days = working_days
+        employe.number_of_employees += 1
+# They are the functions which work in the background
 
-employee_1 = employe("irfan", 22, 6500, "6")
-employee_2 = employe("javed", 38, 7200, "6")
+    # it help us print instance without getting an error
+    def __str__(self):
+        return "name : {} , age : {}".format(self.name, self.age)
+
+    def __repr__(self):
+        return "name : {} , age : {}".format(self.salary, self.working_days)
+    # it is used to add two strings together
+    def __add__(self, n):
+        return 
+
+employee_1 = employe("irfan" , 22, 6500, "6")
+employee_2 = employe("javed" , 38, 7200, "6")
 employee_3 = employe("naseem", 28, 8500, "7")
-employee_4 = employe("shoeb", 26, 7900, "5")
-employee_5 = employe("naved", 42, 8500, "5")
+employee_4 = employe("shoeb" , 26, 7900, "5")
+employee_5 = employe("naved" , 42, 8500, "5")
+print("mohd" + " bilal")
+print(employee_1)
+print(employee_1.__repr__())
 
 print(" _______________________________________________________________")
 print("|               |          |               |                    |")
@@ -24,33 +42,30 @@ print("|_______________|__________|_______________|____________________|")
 class total(employe):
     def __init__(self, name, age, salary, working_days):
         super().__init__(name,age, salary, working_days)
-    
-    @staticmethod
-    def from_max (a, b, c, d, e):
-        p = [a, b, c, d, e]
-        return max(p)
-        
-    @staticmethod
-    def sum_salary (a, b, c, d, e):
-        summ = a + b + c + d + e
+
+    # when you use @property you don't have to use () when calling the function
+    @property    
+    def work(self):
+        summ = employee_1.salary + employee_2.salary + employee_3.salary + employee_4.salary + employee_5.salary
         return summ
+    
+    # when we use (same name as the property func).setter we can set value to a instances
+    # for eg: self.working_days = 28293
+    @work.setter
+    def work(self, name):
+        s = max(name)
+        self.working_days = s
+        
+p = [employee_1.age, employee_2.age, employee_3.age, employee_4.age, employee_5.age]
+v = lambda : max(p)
 
-total_emp = 5
-total_salary = total.sum_salary(employee_1.salary, employee_2.salary, employee_3.salary, employee_4.salary, employee_5.salary)
+state = total((employe.number_of_employees), None, None, None)
 
-max_working_days = total.from_max(employee_1.working_days, employee_2.working_days, employee_3.working_days, employee_4.working_days, employee_5.working_days)
-
-max_employee_age = total.from_max(employee_1.age, employee_2.age, employee_3.age, employee_4.age, employee_5.age)
-
-state = total(total_emp, max_employee_age, total_salary, max_working_days)
-
+lis = [employee_1.working_days, employee_2.working_days ,employee_3.working_days, employee_4.working_days, employee_5.working_days]
+state.work = lis
 print(" __________________________________\n|                      |           |")
 print(f"|   Total Employees :  |  : {state.name}      |".upper())
-print(f"|   Maximum Age     :  |  : {state.age} Yrs |".upper())
-print(f"|   Total Salary    :  |  : ₹{state.salary} |".upper())
+print(f"|   Maximum Age     :  |  : {v()} Yrs |".upper())
+print(f"|   Total Salary    :  |  : ₹{state.work} |".upper())
 print(f"|   Max Working Days:  |  : {state.working_days} Days |".upper())
 print("|______________________|___________|")
-
-
-        
-
